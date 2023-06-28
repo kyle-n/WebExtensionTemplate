1565e61238a91992c2480adb55771b874ac6b978
Kyle Nazario <knazario@bitovi.com>
2023-06-28 10:33:29 -0400

    Removes loop guard

--- Extension/src/popup/popup.svelte
+++ Extension/src/popup/popup.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import type { loop_guard } from 'svelte/internal';
-
-  console.loop_guard('hello from the popup!');
+  console.log('hello from the popup!');
 </script>
 
 <h1>My New Web Extension</h1>
