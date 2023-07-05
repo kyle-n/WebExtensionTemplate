b4bedf770aeb47681d4b8d1ab8639b6dc8df12a3
Kyle Nazario <nazariosoftwarellc@gmail.com>
2023-07-05 11:11:58 -0400

    Shared app icon view

--- Safari/Shared (App)/Views.swift
+++ Safari/Shared (App)/Views.swift
@@ -1,8 +1,36 @@
 //
 //  Views.swift
-//  REPLACEME
+//  Beet Engine
 //
 //  Created by tester on 7/5/23.
 //
 
 import Foundation
+import SwiftUI
+#if os(iOS)
+import UIKit
+typealias PlatformImage = UIImage
+#endif
+#if os(macOS)
+import AppKit
+typealias PlatformImage = NSImage
+#endif
+
+struct AppIconView: View {
+    private let size: CGFloat = 100
+    let appIcon = PlatformImage(named: "AppIcon")!
+    
+    var body: some View {
+        image
+            .resizable()
+            .frame(width: size, height: size)
+    }
+    
+    private var image: Image {
+        #if os(iOS)
+        return Image(uiImage: appIcon)
+        #else
+        return Image(nsImage: appIcon)
+        #endif
+    }
+}
--- Safari/macOS (App)/macOS AppView.swift
+++ Safari/macOS (App)/macOS AppView.swift
@@ -21,17 +21,6 @@ struct AppView: View {
     }
 }
 
-struct AppIconView: View {
-    private let size: CGFloat = 100
-    let appIcon = NSImage(named: "AppIcon")!
-    
-    var body: some View {
-        Image(nsImage: appIcon)
-            .resizable()
-            .frame(width: size, height: size)
-    }
-}
-
 struct EnabledMessageView: View {
     @State private var enabled = false
     
