56a0ebca00d867135565c1c939caf149fb09a537
Kyle Nazario <knazario@bitovi.com>
2023-07-09 16:33:00 -0400

    Excludes packaged zip from source zip

--- Extension/scripts/firefox-build.js
+++ Extension/scripts/firefox-build.js
@@ -8,10 +8,19 @@ main();
 async function main() {
   await exec('npm run build:chrome');
 
+  // Removes non-source files
+  if (fs.existsSync('../tmp')) {
+    await fs.rm('../tmp', { recursive: true });
+  }
   await fs.mkdir('../tmp');
   await fs.move('node_modules', '../tmp/node_modules');
+  await fs.move('packaged-extension.zip', '../tmp/packaged-extension.zip');
+
   zipper.sync.zip('.').compress().save('extension-source.zip');
+
+  // Move project files back
   await fs.move('../tmp/node_modules', 'node_modules');
+  await fs.move('../tmp/packaged-extension.zip', 'packaged-extension.zip');
 
   console.log('Built for Firefox');
 }
