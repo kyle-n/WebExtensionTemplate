4a9c2f7ed631d665b499c5380824c5cf94ecf973
Kyle Nazario <knazario@bitovi.com>
2023-07-09 16:29:10 -0400

    Firefox build script uses chrome build process

--- Extension/scripts/firefox-build.js
+++ Extension/scripts/firefox-build.js
@@ -6,24 +6,7 @@ const zipper = require('zip-local');
 main();
 
 async function main() {
-  await exec('npm run build:prod');
-  const ffDir = 'firefox-extension';
-
-  if (fs.existsSync(ffDir)) {
-    await fs.rm(ffDir, { recursive: true });
-  }
-  await fs.mkdir(ffDir);
-  const zipContents = ['_locales', 'dist', 'images', 'public', 'manifest.json'];
-  for await (const filename of zipContents) {
-    await fs.copy(filename, `${ffDir}/${filename}`);
-  }
-
-  zipper.sync
-    .zip(ffDir)
-    .compress()
-    .save(ffDir + '.zip');
-
-  await fs.rm(ffDir, { recursive: true });
+  await exec('npm run build:chrome');
 
   await fs.mkdir('../tmp');
   await fs.move('node_modules', '../tmp/node_modules');
